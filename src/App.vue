969b321525b049f7e988387d1dbd37e1026f4861
Nico Schreiner <nico@nicoschreiner.de>
2019-04-19 21:20:29 +0200

    Update prettier/prettier config and run ESLint + Prettier

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
@@ -29,7 +29,7 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
       //
     }
